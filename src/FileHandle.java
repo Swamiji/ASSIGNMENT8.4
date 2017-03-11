ab0c86c7def212ec582586210765d5376d456635
Swamiji <amitroy314@gmail.com>
2017-03-11 13:41:36 +0530

    FileHandle
    FileHandle

---
+++ src/FileHandle.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+
+
+public class FileHandle {
+	public static void main(String args[]){
+		try
+		{
+			FileInputStream f=new FileInputStream("D:\\Text\\Text1.txt");
+			int i=0; 
+			while((i=f.read())!=-1){
+				System.out.print((char)i);
+			}
+			f.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+}
